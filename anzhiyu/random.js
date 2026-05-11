bc90cd0dc10ddab9d2a43368feed0cacd1326423
mcwzk <lgwnfxiaoli@gmail.com>
2026-05-11 11:26:37 +0800

    Delete anzhiyu directory

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["/wz/mc/","/wz/gG/","/wz/tp/","/wz/gy/","/wz/mcyxb/","/wz/yinfuhe/","/wz/mssd/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
